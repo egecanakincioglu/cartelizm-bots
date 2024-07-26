10444d674d6680919f572d2a2db3f760917c20c6
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-07-26 14:15:14 +0300

    Delete Database directory

--- Database/Schemas/Afk.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Afk (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    guildID TEXT DEFAULT '',
-    userID TEXT DEFAULT '',
-    reason TEXT DEFAULT '',
-    date INTEGER DEFAULT (strftime('%s', 'now'))
-);
