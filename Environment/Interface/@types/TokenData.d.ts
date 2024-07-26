c30254d1bf1e4bcca20ee6ac16281939c3c22491
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-07-26 14:29:45 +0300

    Token data types added

---
+++ Environment/Interface/@types/TokenData.d.ts
@@ -0,0 +1,17 @@
+interface TokenData {
+    General: {
+        Voucher: string;
+        Supervisor: string;
+        Protectives: {
+            I: string;
+            II: string;
+            III: string;
+        }
+    }
+
+    Welcome: {
+        Enabled: boolean;
+        Tokens: string[];
+        Channels: string[];
+    }
+}
